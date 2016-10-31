0d94dc20749d76036d3f3ad50db3008b813f515e
Yunlong <269138314@qq.com>
2016-10-31 19:46:42 +0800

    添加登录，注册，过场三个布局

---
+++ ChatDemoUI3.0/src/com/hyphenate/chatuidemo/ui/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.hyphenate.chatuidemo.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hyphenate.chatuidemo.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcom);
+    }
+}
