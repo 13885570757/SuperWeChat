8030d23c377e98b602e47668d6d9a619abee7969
Yunlong <269138314@qq.com>
2016-11-03 20:13:08 +0800

    MainActivity修改（部分，天气冷，提前去买被子了。）

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/DiscoverFragment.java
@@ -0,0 +1,28 @@
+package cn.ucai.superwechat.ui;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.superwechat.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DiscoverFragment extends Fragment {
+
+
+    public DiscoverFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_discover, container, false);
+    }
+
+}
