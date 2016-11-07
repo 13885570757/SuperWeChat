d4fba964ca26ea65bba309b436fd7675593fb356
Yunlong <269138314@qq.com>
2016-11-07 19:23:51 +0800

    删除聊天室，聊天助手

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ContactListFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ContactListFragment.java
@@ -185,15 +185,6 @@ public class ContactListFragment extends EaseContactListFragment {
                 // 进入群聊列表页面
                 startActivity(new Intent(getActivity(), GroupsActivity.class));
                 break;
-            case R.id.chat_room_item:
-                //进入聊天室列表页面
-                startActivity(new Intent(getActivity(), PublicChatRoomsActivity.class));
-                break;
-            case R.id.robot_item:
-                //进入Robot列表页面
-                startActivity(new Intent(getActivity(), RobotsActivity.class));
-                break;
-
             default:
                 break;
             }
