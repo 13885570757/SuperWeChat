41db3c5f8978abecd9e5401b8e0a56e47bcb0a64
Yunlong <269138314@qq.com>
2016-11-07 19:59:55 +0800

    搜索到的联系人详情

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/FriendProfileActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+
+import cn.ucai.superwechat.R;
+
+public class FriendProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_profile);
+
+    }
+}
